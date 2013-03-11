5a2e2fd39ed4438379bbc320be9c7dcfcef8d458
OpenShift guest <rosati.matteo@gmail.com>
2013-03-11 09:29:37 -0400

    imported first version from codepen

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Partitures</title>
+    <link rel="stylesheet" type="text/css" href="src/partitures.css">
+</head>
+<body>
+    <h1>Pentagrams</h1>
+    <h2>CSS framework for partitures</h2>
+    
+    <div id="pentagram">
+      <div class="bass clef"></div>
+      <div class="row nine">
+        <div class="beat">
+          <div class="note semibreve sol"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve si bemolle"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve re"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve fa diesis"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve la"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve la low diesis"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve do"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve mi bemolle"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve sol hi"></div>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="bass clef"></div>
+        <div class="beat">
+          <div class="note semibreve mi low bemolle"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve fa low"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve si hi"></div>
+        </div>
+        <div class="beat">
+          <div class="note semibreve do hi diesis"></div>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="bass clef"></div>
+        <div class="beat">
+          <div class="note minima sol"></div>
+          <div class="note minima la"></div>
+        </div>
+        <div class="beat">
+          <div class="note minima si"></div>
+          <div class="note minima do"></div>
+        </div>
+        <div class="beat">
+          <div class="note minima re"></div>
+          <div class="note minima mi"></div>
+        </div>
+        <div class="beat">
+          <div class="note minima fa"></div>
+          <div class="note minima sol hi"></div>
+        </div>
+      </div>
+    </div>
+</body>
